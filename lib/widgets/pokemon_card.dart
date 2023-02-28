639a9553c220c9e53cb7aa1eac3c34a30f96cb16
Carlos Antonio Martín Durán <carlos.martindu@gmail.com>
2023-02-28 20:16:26 +0000

    añadido efectos con animate_do

--- lib/widgets/pokemon_card.dart
+++ lib/widgets/pokemon_card.dart
@@ -1,4 +1,5 @@
 
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:pokemon_api/providers/pokemon_provider.dart';
 import 'package:provider/provider.dart';
@@ -18,41 +19,49 @@ class PokemonCard extends StatelessWidget {
       future: pokemonProvider.getPokemonData(name),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.hasData) {
-          return Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: Row(
-              children: [
-                Image.network(snapshot.data.sprites.frontDefault ?? '', height: 100),
-                Expanded(
-                  child: Text(
-                    "${snapshot.data.name[0].toUpperCase()}${snapshot.data.name.substring(1).toLowerCase()}",
+          return ZoomIn(
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Row(
+                children: [
+                  Image.network(snapshot.data.sprites.frontDefault ?? '', height: 100),
+                  Expanded(
+                    child: Text(
+                      "${snapshot.data.name[0].toUpperCase()}${snapshot.data.name.substring(1).toLowerCase()}",
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const Expanded(child: SizedBox()),
+                  Text(
+                    '#${snapshot.data.id}',
                     style: const TextStyle(
                       fontSize: 18,
-                      fontWeight: FontWeight.bold
+                      fontWeight: FontWeight.bold,
+                      
                     ),
-                    textAlign: TextAlign.center,
+          
                   ),
-                ),
-                const Expanded(child: SizedBox()),
-                Text(
-                  '#${snapshot.data.id}',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    
-                  ),
-
-                ),
-              ],
+                ],
+              ),
             ),
           );
         } else {
-          return const Center(child: CircularProgressIndicator());
+          return Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            child: const Center(
+              child: CircularProgressIndicator()
+            ),
+          );
         }
       },
     );
